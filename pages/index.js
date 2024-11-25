733700dad9c798b591845a7d402aa7da34e93b64
Thierry <thierryjefersson@gmail.com>
2024-11-25 12:28:39 -0300

    adiciona emoji a frase do pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1>Hoje em dia, as pessoas sabem o preço de tudo e o valor de nada.</h1>
+    <h1>
+      As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o
+      efeito do banho, por isso recomenda-se diariamente. 😆
+    </h1>
   );
 }
