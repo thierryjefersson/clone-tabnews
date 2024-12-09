2e347833b2c58a94a9a5ba9bd2db952a8e2c007e
Thierry <thierryjefersson@gmail.com>
2024-12-09 20:49:09 -0300

    change `test runner` to Jest

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,3 @@
-import { expect, test } from "vitest";
-
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
