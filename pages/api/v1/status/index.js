9711b248d63c7ce4d518ead1bf28c51d9db7d50c
Thierry <thierryjefersson@gmail.com>
2024-12-10 11:55:40 -0300

    add file `.env`, `database.js` and install `pg`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-export default function status(req, res) {
+import database from "../../../../infra/database.js";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows[0]);
+
   return res.status(200).json({
     chave: "Primeiro endpoint",
   });
