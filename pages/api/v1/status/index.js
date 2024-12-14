ca176ff10753b17a8869a0f538abcc1c40983fd6
Thierry <thierryjefersson@gmail.com>
2024-12-14 16:45:46 -0300

    finish `api/v1/status` status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,26 @@
 import database from "infra/database.js";
 
-export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0]);
+export default async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
 
-  return res.status(200).json({
-    chave: "Primeiro endpoint",
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        opened_connections: databaseOpenedConnections.rows[0].count,
+      },
+    },
   });
 }
